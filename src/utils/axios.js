87119e44488ed945830f9985fc0b4a878d7b472a
Aditya12214307 <adityatanty0@gmail.com>
2025-07-18 10:34:11 +0530

    Updated API baseURL with env variable

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URl || "http://localhost:3000/api";
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 const API = axios.create({
   baseURL: apiUrl,
